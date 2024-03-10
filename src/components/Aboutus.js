81832fe332bab54f9454962db632ae49d4d5b182
Sujal Sharma <sharmasujal9493@gmail.com>
2024-03-10 22:16:24 +0530

    commit

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -9,11 +9,14 @@ export const Aboutus = () => {
     >
       <h1 className="text-6xl font-bold font-[Play]">About Us</h1>
       <p className="max-w-4xl text-center text-xl my-16 px-6 py-4 border-solid-white border-2 rounded-xl">
-      C-SOC is the Society of Civil Engineering at NIT Hamirpur, known for its standout and hardworking members amongst the college fraternity.
+        C-SOC is the Society of Civil Engineering at NIT Hamirpur, known for its
+        standout and hardworking members amongst the college fraternity.
       </p>
       <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-        <img src={logo} alt="logo" className="h-36 md:h-48"/>
+        <img src={logo} alt="logo" className="h-36 md:h-48" />
       </div>
     </div>
   );
 };
+
+// about us page
