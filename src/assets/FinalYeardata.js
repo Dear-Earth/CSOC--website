8253fd28db8c967baa7eaf040b6beed5c1ee03ae
Sujal Sharma <sharmasujal9493@gmail.com>
2025-04-05 21:46:36 +0530

    update the post of members of team

--- src/assets/FinalYeardata.js
+++ src/assets/FinalYeardata.js
@@ -1,75 +1,68 @@
-import Charitra from "../assets/FinalYearImages/IMG_0515 - Charitra Pradhan.jpeg"
-import Garima from "../assets/FinalYearImages/338631926_3443246075900306_5831566410241566853_n - Garima Dahiya.jpg"
-import Tarun from "../assets/FinalYearImages/IMG-20231006-WA0124 - Tarun Agrawal.jpg"
-import Pasang from "../assets/FinalYearImages/my-passport-photo - Pasang Dolma.jpg"
-import Tanmay from "../assets/FinalYearImages/Screenshot_20230321_012057 - 20BCE002_Tanmay Goyal.jpg"
-import Images from "../assets/aImages/pngwing.com.png"
+import Images from "./aImages/pngwing.com.png";
+import Arpit from "./aImages/arpit.jpg";
+import Jigyasa from "../assets/ThirdYearImages/IMG_9825 - JigyasaAnand.JPG"
+import Shalu from "../assets/ThirdYearImages/IMG_20230706_200423_058 - ShaluRana.jpg"
+import Saksham from "../assets/ThirdYearImages/sakshamsharm.jpg"
 
 const data = [
   {
-    name: `Tanmay Goyal`,
-    post: `President`,
-    img: Tanmay,
+    name: `Saksham Sharma`,
+    post:`President`,
+    img: Saksham,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Pasang Dolma`,
-    post: `Vice - President`,
-    img: Pasang,
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    name: `Tarun Agrawal`,
-    post: `Vice - President`,
-    img: Tarun,
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    name: `Prakhar Veer Singh`,
-    post: `Website Head`,
+    name: `Aradhya Kapoor`,
+    post:`Vice President`,
     img: Images,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Charitra Pradhan`,
-    post: `Treasurer`,
-    img: Charitra,
+    name: `Shalu Rana`,
+    post:`Vice President`,
+    img: Shalu,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `DivyaShri Veligotla`,
-    post: `Media Secretary`,
+    name: `Parul Kajal`,
+    post:`Treasurer`,
     img: Images,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Garima`,
-    post: `Technical Secretary`,
-    img: Garima,
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    name: `Himanshu Sharma`,
-    post: `Editor in cheif Aadhar`,
+    name: `Shriya`,
+    post:`PR Secretary`,
     img: Images,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Kartik`,
-    post: `Finance Secretary`,
+    name: `Pranav Veer Singh`,
+    post:`Editor in Chief Aadhar`,
     img: Images,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Surbhi`,
-    post: `PR Secretary`,
+    name: `Aditi Tiwari`,
+    post:`Finance Secretary`,
     img: Images,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Unique Chander Azad`,
-    post: `Discipline Secretary`,
+    name: `Somanshu Mehra`,
+    post:`Technical Secretary`,
     img: Images,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Jigyasa`,
+    post:`Media Secretary`,
+    img: Jigyasa,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Arpit`,
+    post:`Website Head`,
+    img: Arpit,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
 ];
--- src/assets/ThirdYeardata.js
+++ src/assets/ThirdYeardata.js
@@ -1,75 +1,52 @@
-import Images from "./aImages/pngwing.com.png";
-import Arpit from "./aImages/arpit.jpg";
-import Pritanjali from "../assets/ThirdYearImages/IMG_8962 - PritanjaliSen.JPG"
-import Jigyasa from "../assets/ThirdYearImages/IMG_9825 - JigyasaAnand.JPG"
-import Shalu from "../assets/ThirdYearImages/IMG_20230706_200423_058 - ShaluRana.jpg"
-import Saksham from "../assets/ThirdYearImages/sakshamsharm.jpg"
+import Shaurya from "../assets/SecondYearImages/22BCE095 - Shaurya Sharma.jpg";
+import Arnav from "../assets/SecondYearImages/IMG_20220612_181905-min - Arnav Chauhan.jpeg";
+import SRIJAN from "../assets/SecondYearImages/IMG_20231218_142309 - SRIJAN GAUTAM.jpg";
+import SATYA from "../assets/SecondYearImages/IMG-20230131-WA0006 - SATYA PANDEY.jpg";
+import Jashanpreet from "../assets/SecondYearImages/IMG-20240114-WA0000 - Jashanpreet Singh.jpg";
+import Dikshant from "../assets/SecondYearImages/passportsizeimg (1) - Dikshant Mehra.jpg";
+import Vanshika from "../assets/SecondYearImages/Snapchat-1501926945 - Vanshika Agrawal.jpg";
 
 const data = [
   {
-    name: `Aradhya Kapoor`,
-    post:`Activities Coordinator`,
-    img: Images,
+    name: `Shaurya Sharma`,
+    post: `Activity coordinator`,
+    img: Shaurya,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Saksham Sharma`,
-    post:`Finance Coordinator`,
-    img: Saksham,
+    name: `Arnav Chauhan`,
+    post: `Activity coordinator`,
+    img: Arnav,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Arpit`,
-    post:`Web & Tech Coordinator`,
-    img: Arpit,
+    name: `Srijan Gautam`,
+    post: `Media coordinator`,
+    img: SRIJAN,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Somanshu Mehra`,
-    post:`PR Coordinator`,
-    img: Images,
+    name: `Satya Kumari`,
+    post: `Finance coordinator`,
+    img: SATYA,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Parul Kajal`,
-    post:`Finance Coordinator`,
-    img: Images,
+    name: `Jashanpreet Singh`,
+    post: `Media coordinator`,
+    img: Jashanpreet,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Pranav Veer Singh`,
-    post:`Design Coordinator`,
-    img: Images,
+    name: `Dikshant Mehra`,
+    post: `PR coordinator`,
+    img: Dikshant,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
   {
-    name: `Pritanjali Sen`,
-    post:`Activities Coordinator`,
-    img: Pritanjali,
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    name: `Aditi Tiwari`,
-    post:`Media Coordinator`,
-    img: Images,
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    name: `Jigyasa`,
-    post:`Design Coordinator`,
-    img: Jigyasa,
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    name: `Shalu Rana`,
-    post:`Media Coordinator`,
-    img: Shalu,
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    name: `Shriya`,
-    post:`PR Coordinator`,
-    img: Images,
+    name: `Vanshika Agrawal`,
+    post: `Design Coordinator`,
+    img: Vanshika,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   },
 ];
